1eb4b107133b729c6197b3b2b4ce32de07b0d6ec
YuanLin <2752137708@qq.com>
2023-09-27 16:00:41 +0800

    修改入口文件

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import '@/styles/reset.less'
-
-export default function App() {
-  return <div>hello React + Webpack</div>
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from '@/App'
+import '@/styles/reset.less'
+
+function App() {
+  return <div>hello React + Webpack</div>
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
